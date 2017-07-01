06ecc31699a386014af3a2f4355bf36aef6b1116
Ralf Walk <ralf.walk@allianz.de>
2017-07-01 10:00:27 +0200

    Layout + value evaluation in new code

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <DynamicProgramming />
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Dynamic Programming</h1>
         </div>
+        <DynamicProgramming />
       </div>
     );
   }
--- src/dynamic-programming/dynamic_programming.js
+++ src/dynamic-programming/dynamic_programming.js
@@ -4,14 +4,26 @@ import React from 'react';
 function DynamicProgramming() {
   return (
     <div className="DynamicProgramming">
-        <h1>DynamicProgramming with gridworld example</h1>
         <h2>Policy Evaluation</h2>
         <table> {
             doIterativePolicyEvaluation(mdp).map(function(value) {
                 return (
                     <tr>
                         {value.map(function(v) {
-                            return <th>{value}</th>
+                            return <th>{v}</th>
+                        })}
+                    </tr>
+                )
+            })
+        }
+        </table>
+        <h2>Value Evaluation</h2>
+        <table> {
+            doIterativeValueEvaluation(mdp).map(function(value) {
+                return (
+                    <tr>
+                        {value.map(function(v) {
+                            return <th>{v}</th>
                         })}
                     </tr>
                 )
@@ -49,7 +61,7 @@ function doIterativePolicyEvaluation(mdp) {
     let value_function = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
 
     // at each iteration k + 1
-    for (let k = 0; k < 4; k++) {
+    for (let k = 0; k < 2; k++) {
         console.log("Iteration: " + k + " Current Value function " + value_function);
         value_function = doPolicyEvaluation(mdp, value_function);
     }
@@ -59,32 +71,30 @@ function doIterativePolicyEvaluation(mdp) {
 function doPolicyEvaluation(mdp, value_function) {
     let new_value_function = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
 
-        // for all states
-        for (let i = 0;  i <= 3; i++) {
-            for (let j = 0; j <= 3; j ++) {
+    // for all states
+    for (let i = 0;  i <= 3; i++) {
+        for (let j = 0; j <= 3; j ++) {
 
-                let state = [i,j];
+            let state = [i,j];
 
-                let new_value = 0;
+            let new_value = 0;
 
-                let actions = mdp.action(state);
+            let actions = mdp.action(state);
 
-                for (let action of  actions) {
+            for (let action of  actions) {
 
-                    // get the reward for the state transition
-                    let reward = mdp.reward(state, action)
-                    
-                    let state_prime_value = value_function[action[0]][action[1]];
-
-                    // calculate the value of state by adding the reward for the transition plus the cached value from the next state
-                    new_value += (reward + state_prime_value) * 0.25;
-                    console.log("BAUM STATE " + state + " REWARD " + reward + " VALUE "+ state_prime_value + " ACTION " + action + " NEW " + (reward + state_prime_value) * 0.25);
+                // get the reward for the state transition
+                let reward = mdp.reward(state, action)
                 
-                }
-                console.log ("NEW ACTIOn ######## ", new_value);
-                new_value_function[state[0]][state[1]] = new_value;
+                let state_prime_value = value_function[action[0]][action[1]];
+
+                // calculate the value of state by adding the reward for the transition plus the cached value from the next state
+                new_value += (reward + state_prime_value) * 0.25;
+                //console.log("BAUM STATE " + state + " REWARD " + reward + " VALUE "+ state_prime_value + " ACTION " + action + " NEW " + (reward + state_prime_value) * 0.25);   
             }
+            new_value_function[state[0]][state[1]] = new_value;
         }
+    }
 
     return new_value_function;
 }
@@ -94,53 +104,42 @@ function doIterativeValueEvaluation(mdp) {
     let value_function = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
 
     // at each iteration k + 1
-    for (let k = 0; k < 7; k++) {
+    for (let k = 0; k < 4; k++) {
         console.log("Iteration: " + k + " Current Value function " + value_function);
         value_function = doValueEvaluation(mdp, value_function);
     }
+    console.log("VVV ", value_function)
     return value_function;
 }
 
 function doValueEvaluation(mdp, value_function) {
-    let new_value_function = value_function.slice(0);
+    let new_value_function = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
 
-        // for all states s
-        for (let state_index = 0;  state_index < mdp.environment.length; state_index++) {
+    // for all states
+    for (let i = 0;  i <= 3; i++) {
+        for (let j = 0; j <= 3; j ++) {
 
-            let old_value = value_function[state_index];
+            let state = [i,j];
 
             let new_value;
 
-            if (mdp.environment[state_index] == 0) {
-                // exit if we are in a terminal state we do not have any action and get 0 reward
-                new_value = 0;
-            } else {
-                // update v_k+1(s) from v_k(s'), where s' is a successor of s
-                let environment_length = mdp.environment.length;
-                let action_length = mdp.action.length;
+            let actions = mdp.action(state);
 
+            for (let action of  actions) {
 
-                for (let action of  mdp.action) {
+                // get the reward for the state transition
+                let reward = mdp.reward(state, action)
+                
+                let state_prime_value = value_function[action[0]][action[1]];
 
-                    // get the reward for the state transition
-                    let reward = mdp.reward(state_index, action)
-        
-                    let state_index_prime = state_index;
-                    if ((state_index + action < environment_length) && (state_index + action >= 0)) {
-                        // we are in a new state
-                        state_index_prime = state_index + action;
-                    }
-                    let state_prime_value = value_function[state_index_prime];
-
-                    if (!new_value || new_value < reward + state_prime_value) {
-                        new_value = reward + state_prime_value;
-                    }
-                }
+                // calculate the value of state by adding the reward for the transition plus the cached value from the next state
+                if (!new_value || new_value < (reward + state_prime_value)) {
+                    new_value = (reward + state_prime_value);
+                }  
             }
-
-            new_value_function[state_index] = new_value;
+            new_value_function[state[0]][state[1]] = new_value;
         }
-
+    }
     return new_value_function;
 }
 
